93829c04935b6d888b614b9ac2cb441b1add692e
tuankiet0811 <kiettn2k4@gmail.com>
2025-06-18 09:39:39 +0700

    Tao view login

---
+++ UserAccount/UserAccount/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UserAccount.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
